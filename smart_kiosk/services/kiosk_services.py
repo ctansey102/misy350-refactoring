3438584679bc40bcc524605cc2bd653f804bab05
Chris <ctansey@udel.edu>
2026-04-20 10:20:59 -0400

    smartkiosk

--- smart_kiosk/services/kiosk_services.py
+++ smart_kiosk/services/kiosk_services.py
@@ -1,38 +1,54 @@
 import uuid
+from typing import List, Dict, Optional
 
-
-
-
-
-
-
-
-def place_order(inventory_items : list, item_id : str, quantity : int):
-    #find item 
+def place_order(inventory_items : list, item_id : str, quantity: int, orders: list) -> Optional[Dict]:
     item = find_inventory_item_by_item_id(inventory_items, item_id)
-    #check the existing inventory for the item.
     if item:
-        if item['quantity'] >= quantity:
-            orders.append(
-                {
-                    "order_id": str(uuid.uuid4())
-                }
-            )
-        #if we have enough inventory, then reduce the inventory
-        #then place the order
+        if item['stock'] >= quantity:
+            item['stock'] = item["stock"] - quantity #reduce the stock
 
-def find_orders_by_item():
+            total = quantity * item['unit_price']
+
+            # create the new order dict
+            new_order = {
+                "order_id": str(uuid.uuid4()),
+                "item_id": item_id,
+                "quantity": quantity,
+                "status": "placed",
+                "total": total
+            }
+            #add the new order to the orders
+            orders.append(new_order)
+            return new_order
+
+
+def find_item_names(inventory: List) -> List:
+    item_names = []
+    for item in inventory:
+        item_names.append(item['name'])
+    return item_names
+
+def find_orders_by_item_id():
     pass
 
 def count_orders_by_item_id():
     pass
 
-def find_inventory_item_by_item_id(inventory_list: list, item_id : str):
-    for item in inventory_list:
-        if item['id'] == item_id:
+def find_inventory_item_by_item_id(invetory_list: list, item_id : str) -> Optional[Dict]:
+    for item in invetory_list:
+        if item['item_id'] == item_id:
             return item
-    return None
     
+    return None
+
+def update_invetory_item():
+    pass
+
+def add_new_item_to_inventory():
+    pass
 
 def update_inventory_item():
+    pass
+
+def cancel_order():
     pass
