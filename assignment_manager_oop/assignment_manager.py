fffe9b4463eb6b2e3b98dc2306143a647f573e56
Chris <ctansey@udel.edu>
2026-04-24 11:00:06 -0400

    oop

--- assignment_manager_oop/assignment_manager.py
+++ assignment_manager_oop/assignment_manager.py
@@ -1,4 +1,5 @@
 from typing import List, Dict, Optional
+import uuid
 
 class AssignmentManager:
     def __init__(self,intial_assignments) -> None:
@@ -9,7 +10,22 @@ class AssignmentManager:
 
     def add(self, title: str , description: str,
             points: int , assignment_type: str):
-        pass
+        if not title.strip():
+            raise ValueError("Title is required")
+        
+        allowed_types = ["Homework", "Lab"]
+
+        if assignment_type.lower() not in allowed_types:
+            raise ValueError("assignment type is invalid")
+        
+        new_assignment = {
+            "id": str(uuid.uuid4()),
+            "title": title,
+            "description": description,
+            "points": points,
+            "type": assignment_type.lower()
+        }
+        self.assignments.append(new_assignment)
 
     def delete(self, assignment_id: str):
-        pass
+        self.assignments = [assignment for assignment in self.assignments if assignment["id"] != assignment_id]
